d53ba613e474ad47f3bb07c0d4ec1e93b0eb962a
hotsno <71658949+hotsno@users.noreply.github.com>
2025-01-27 06:11:00 -0800

    Switch to numbered files

--- scripts/rename_files.py
+++ scripts/rename_files.py
@@ -1,43 +1,53 @@
 import os
 import re
-import random
-import string
 
-# Function to generate a random string of length 8
-def generate_random_string(length=8):
-    return ''.join(random.choices(string.ascii_lowercase + string.digits, k=length))
+valid_filename_pattern = re.compile(r'^[0-9]{3}$')
 
-# Regex pattern to match valid filenames
-valid_filename_pattern = re.compile(r'^[a-z0-9]{8}$')
-
-os.chdir("images")
-
-# Rename files in the directory
-def rename_files():
+# Get last properly named file number
+def get_last_properly_named():
+    res = 0
     for filename in os.listdir("."):
-        # Skip directories
         if os.path.isdir(filename):
             continue
 
-        # Get the file extension
         name, ext = os.path.splitext(filename)
-
-        # Skip non-image files
         if ext.lower() not in [".png", ".jpg", ".jpeg", ".webp"]:
             print(f"Skipping file: {filename}")
             continue
 
-        # Check if the filename matches the valid pattern
-        if not valid_filename_pattern.fullmatch(name):
-            # Generate a new random filename
-            new_name = generate_random_string()
-            while os.path.exists(f"{new_name}{ext}"):
-                new_name = generate_random_string()
+        if valid_filename_pattern.fullmatch(name):
+            res = max(res, int(name))
 
-            # Rename the file
-            os.rename(filename, f"{new_name}{ext}")
-            print(f"Renamed: {filename} -> {new_name}{ext}")
+    return res
+
+# Rename files in the directory
+def rename_files(dir):
+    os.chdir(dir)
+
+    start = get_last_properly_named() + 1
+
+    for filename in os.listdir("."):
+        if os.path.isdir(filename):
+            continue
+
+        name, ext = os.path.splitext(filename)
+
+        if ext.lower() not in [".png", ".jpg", ".jpeg", ".webp"]:
+            print(f"Skipping file: {filename}")
+            continue
+
+        if not valid_filename_pattern.fullmatch(name):
+            new_filename = str(start).zfill(3) + ext
+            start += 1
+
+            if os.path.exists(new_filename):
+                raise ValueError(f"Unexpected state: {new_full_filename} already exists")
+
+            os.rename(filename, new_filename)
+            print(f"Renamed: {filename} -> {new_filename}")
+
+    os.chdir("..")
 
 if __name__ == "__main__":
-    rename_files()
+    rename_files("images")
 
